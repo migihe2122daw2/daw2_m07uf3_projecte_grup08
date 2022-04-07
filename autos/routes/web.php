0b9bc2f9326fa9631b70c202413672c2dd2e49dc
migihe <migihe2122daw2@protonmail.com>
2022-04-07 17:18:14 +0200

    Commit final

--- autos/routes/web.php
+++ autos/routes/web.php
@@ -89,6 +89,11 @@ Route::post('/loginAdmin', function (Request $request) {
                 
                 $mail->send();
                 echo 'Message has been sent';
+
+                // Guardar hora de entrada en la base de dades
+                $user = Auth::user();
+                $user-> Darrera_hora_de_entrada = date('H:i:s');
+                $user->save();
             } catch (Exception $e) {
                 echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
             }
@@ -111,9 +116,62 @@ Route::get('/treballador', function () {
 })->name('treballador');
 
 Route::get('/logout', function () {
+    // Enviar correu amb la hora de sortida
+    $mail = new PHPMailer(true);
+    try {
+        //Server settings
+        $mail->SMTPDebug = 0;                                       // Enable verbose debug output
+        $mail->isSMTP();                                            // Set mailer to use SMTP
+        $mail->Host       = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+        $mail->Username   = '15586419.clot@fje.edu';                     // SMTP username
+        $mail->Password   = 'cce07h5f';                               // SMTP password
+        $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
+        $mail->Port       = 587;                                    // TCP port to connect to
+
+        //Recipients
+        $mail->setFrom('15586419.clot@fje.edu', 'Clot');
+        $mail->addAddress('15586419.clot@fje.edu', 'Clot');     // Add a recipient
+    
+        // Hora española
+        date_default_timezone_set('Europe/Madrid');
+
+        // Content
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = 'Sortida';
+        $mail->Body    = '<h1>Hola, ' . Auth::user()->Nom_i_cognoms . '</h1>
+                          <p>Has sortit de la plataforma</p>
+                          <p>Hora: ' . date('H:i:s') . '</p>';
+        $mail->AltBody = 'Hola, ' . Auth::user()->Nom_i_cognoms . 'Has sortit de la plataforma, hora: ' . date('H:i:s');
+
+        $mail->send();
+        echo 'Message has been sent';
+
+        // Guardar hora de sortida en la base de dades
+        $user = Auth::user();
+        $user->Darrera_hora_de_sortida = date('H:i:s');
+        $user->save();
+
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+
     Auth::logout();
     return redirect('/');
 });
+
+Route::get('/eliminar', function () {
+    // Comprovar que estem loguejats com a cap de departament
+    if (Auth::user()->Tipus_de_usuari == 'Cap de departament') {
+        // Eliminar totes les dades de les taules
+        DB::table('lloguers')->delete();
+        DB::table('autos')->delete();
+        DB::table('clients')->delete();
+        return redirect('/');
+    }else
+        return redirect('/');
+});
+
 
 Route::get('/autos/{id}/pdf', 'ControladorAutos@pdf')->name('autos.pdf');
 Route::get('/clients/{id}/pdf', 'ControladorClients@pdf')->name('clients.pdf');
